c887976b3d9fee2332ef48597205f3fad243ce44
tyy <tanyunyun@plantdata.cn>
2023-02-01 13:54:38 +0800

    feat: 跳转页面初始化

--- src/hooks/useWhere2go.tsx
+++ src/hooks/useWhere2go.tsx
@@ -1,5 +1,28 @@
+import { history } from 'umi';
 const useWhere2go = () => {
-  return {};
+  /**
+   * 跳转集市板块
+   * @param currentKey 板块key
+   */
+  const goMarketDetail = (currentKey: string) => {
+    history.push({
+      pathname: '/market/home/detail',
+      query: { currentKey },
+    });
+  };
+
+  /**
+   * 跳转首页
+   */
+  const goHome = () => {
+    history.push({
+      pathname: '/market/home',
+    });
+  };
+  return {
+    goHome,
+    goMarketDetail,
+  };
 };
 
 export default useWhere2go;
