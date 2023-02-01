2876ac9f860be0e2040a9ecc3e64c1e151ea0c8e
tyy <tanyunyun@plantdata.cn>
2023-02-01 13:55:23 +0800

    feat: 首页初始化

---
+++ src/pages/market/home/detail/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+import useUrlState from '@ahooksjs/use-url-state';
+import { FloatButton } from 'antd';
+import { EditOutlined, HomeOutlined } from '@ant-design/icons';
+import useWhere2go from '@/hooks/useWhere2go';
+import './index.less';
+
+import { useEffect } from 'react';
+
+const Index: React.FC = () => {
+  const [state, setState] = useUrlState();
+
+  const [currentKey, setCurrentKey] = useState<string>(
+    state?.currentKey ?? 'all',
+  );
+
+  const { goHome } = useWhere2go();
+
+  const items: TabsProps['items'] = [
+    {
+      key: 'all',
+      label: `全部`,
+      children: <div>全部</div>,
+    },
+    {
+      key: 'usedIdle',
+      label: `二手闲置`,
+      children: <div>二手闲置</div>,
+    },
+    {
+      key: 'askForHelp',
+      label: `打听求助`,
+      children: <div>打听求助</div>,
+    },
+    {
+      key: 'loveMakeFriends',
+      label: `恋爱交友`,
+      children: <div>恋爱交友</div>,
+    },
+    {
+      key: 'interestFacts',
+      label: `瓜田趣事`,
+      children: <div>瓜田趣事</div>,
+    },
+  ];
+
+  const onChange = (key: string) => {
+    console.log(key);
+    setCurrentKey(key);
+    setState({ currentKey: key });
+  };
+
+  useEffect(() => {
+    setCurrentKey(state?.currentKey);
+  }, [state?.currentKey]);
+
+  return (
+    <div className="md_market-detail">
+      <Tabs activeKey={currentKey} items={items} onChange={onChange} />
+      <FloatButton.Group
+        shape="square"
+        style={{ right: 20, backGroundColor: 'red' }}
+      >
+        <FloatButton icon={<EditOutlined />} description={'发布'} />
+        <FloatButton
+          icon={<HomeOutlined />}
+          description={'首页'}
+          onClick={goHome}
+        />
+      </FloatButton.Group>
+    </div>
+  );
+};
+export default Index;
--- src/pages/market/home/index.tsx
+++ src/pages/market/home/index.tsx
@@ -1,4 +1,66 @@
+import Footer from '@/layouts/components/footer';
+import userIdleImg from '@/assets/images/market-module-userIdle.png';
+import backgroundImg from '@/assets/images/market-background.png';
+import helpImg from '@/assets/images/market-module-help.png';
+import loveImg from '@/assets/images/market-module-love.png';
+import factsImg from '@/assets/images/market-module-facts.png';
+import addImg from '@/assets/images/home-add.png';
+import { Image, FloatButton } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons';
+import './index.less';
+import useWhere2go from '@/hooks/useWhere2go';
 const Index = () => {
-  return <div>集市</div>;
+  const { goMarketDetail } = useWhere2go();
+
+  const marketModuleItems = [
+    {
+      key: 'usedIdle',
+      icon: userIdleImg,
+      label: '二手闲置',
+    },
+    {
+      key: 'askForHelp',
+      icon: helpImg,
+      label: '打听求助',
+    },
+    {
+      key: 'loveMakeFriends',
+      icon: loveImg,
+      label: '恋爱交友',
+    },
+    {
+      key: 'interestFacts',
+      icon: factsImg,
+      label: '瓜田趣事',
+    },
+  ];
+  return (
+    <div className="md__market-home">
+      <header className="home-header">
+        <Image src={backgroundImg} preview={false} />
+      </header>
+      <section className="market-module">
+        {marketModuleItems.map(({ key, icon, label }) => (
+          <div
+            key={key}
+            className="module-item"
+            onClick={() => {
+              goMarketDetail(key);
+            }}
+          >
+            <Image src={icon} preview={false} />
+            <p>{label}</p>
+          </div>
+        ))}
+      </section>
+      <main className="market-post">帖子列表</main>
+      <Footer />
+      <FloatButton
+        icon={<PlusCircleFilled style={{ color: '#43ba9d' }} />}
+        style={{ bottom: 100 }}
+        onClick={() => console.log('click')}
+      />
+    </div>
+  );
 };
 export default Index;
