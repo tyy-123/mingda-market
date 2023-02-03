8ef0141a73c081c20ad7c0db53eb12d1a5de30be
tyy <tanyunyun@plantdata.cn>
2023-02-03 15:33:45 +0800

    feat: 发布modal框初始化

---
+++ src/components/postModal/index.tsx
@@ -0,0 +1,38 @@
+import { BaseComponentProps } from '@/interface';
+import { Button, Modal, Upload, message } from 'antd';
+import './index.less';
+
+export interface PostModalProps extends BaseComponentProps {
+  visible: boolean;
+  onCancel: () => void;
+}
+const PostModal: React.FC<PostModalProps> = ({
+  visible,
+  onCancel,
+  ...rest
+}) => {
+  const columnTypes = ['二手闲置', '打听求助', '恋爱交友', '瓜田趣事'];
+  return (
+    <Modal
+      width={300}
+      className="md__post-modal"
+      title="选择栏目"
+      {...rest}
+      closable={false}
+      open={visible}
+      destroyOnClose={true}
+      maskClosable={false}
+      centered
+      footer={
+        <div className="modal-footer" onClick={onCancel}>
+          取消
+        </div>
+      }
+    >
+      {columnTypes.map((type: string) => (
+        <div className="type-item">{`${type}  >`}</div>
+      ))}
+    </Modal>
+  );
+};
+export default PostModal;
