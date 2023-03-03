b15fbba38263fbff0c8badd8a8a8a9084b4f7ceb
tyy <tanyunyun@plantdata.cn>
2023-03-03 14:04:53 +0800

    feat: 注册登录接口更新

---
+++ src/services/api/user.ts
@@ -0,0 +1,22 @@
+const user={
+  getCode_get:{
+    url: '/getCode',
+    options: {
+      method: 'GET',
+    },
+  },
+  register_post:{
+    url: '/register',
+    options: {
+      method: 'POST',
+    },
+  },
+  login_get:{
+    url: '/login',
+    options: {
+      method: 'GET',
+    },
+  },
+
+}
+export { user };
