9239653d728c6ed6a8e4f4adc93673f07f7d11cd
tyy <tanyunyun@plantdata.cn>
2023-03-03 11:33:13 +0800

    feat: context初始化

---
+++ src/lang/index.ts
@@ -0,0 +1,34 @@
+const MdLanguage = {
+  zhCN: {
+    user: {
+      welcome: '欢迎您',
+      quit: '退出',
+    },
+    baseLayout: {
+      manageBtn: '后台管理',
+    },
+    login: {
+      text: '登录',
+      welcome: '欢迎登录',
+      way: '账户密码登录',
+      autoLogin: '自动登录',
+      register: '立即注册',
+      forget: ' 忘记密码',
+    },
+    register: {
+      text: '注册',
+      welcome: '欢迎注册',
+      register: '立即注册',
+      login: '立即登录',
+      hasAccount: '已有账号',
+      codeError: '获取验证码错误',
+      emailError: '邮箱格式错误',
+      pwdError: '密码至少包含一个数字且不小于6个字符',
+      phoneError: '请输入正确的手机号',
+      nameError: '姓名格式错误',
+    },
+  },
+  enUS: {},
+};
+
+export default MdLanguage;
