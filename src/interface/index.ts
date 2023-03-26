95086f65feabdbcd3aa8a9563901215382860790
tyy <tanyunyun@plantdata.cn>
2023-03-26 15:52:10 +0800

    feat: 模块类型接口添加

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -7,3 +7,24 @@ export interface PageConfig {
   current: number;
   pageSize: number;
 }
+
+export interface NoteMsg {
+  avatar: string;
+  commentCount: number;
+  content: string;
+  imgs: string;
+  modelId: number;
+  noteId: number;
+  releaseTime: string;
+  userId: number;
+  username: string;
+}
+
+export enum ModelType {
+  USEDIDLE = 0, //二手闲置
+  ASKFORHELP,//打听求助
+  LOVEMAKEFRIENDS,//恋爱交友
+  INTERESTFACTS,//瓜田趣事
+  TEACHERMSGS,//兼职信息
+  SCHOOLWORK,//校园招聘
+}
